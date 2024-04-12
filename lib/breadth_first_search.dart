3feaa08aeb11559fb2ebbb432a88f031b2cf04bb
SaifSamir1 <saifsamirsaif12@gmail.com>
2024-04-12 03:43:20 +0200

    Breadth First Search Algorithm

--- lib/breadth_first_search.dart
+++ lib/breadth_first_search.dart
@@ -1,45 +1,68 @@
 import 'dart:collection';
 
-class Node {
-  final int value;
-  final List<Node> neighbors;
-  const Node(this.value, this.neighbors);
-}
-
-List<int> bfs(Node startNode) {
+List<int> bfs(Map<int, List<int>> graph, int startNode, int destinationNode) {
   final visited = Set<int>(); // Keep track of visited nodes
-  final queue = Queue<Node>(); // Queue for BFS traversal
+  final queue = Queue<int>(); // Queue for BFS traversal
 
   queue.add(startNode);
-  visited.add(startNode.value);
+  visited.add(startNode);
 
-  final List<int> result = [];
+  final previousNode = Map<int, int>(); // Track the path for each visited node
+
   while (queue.isNotEmpty) {
     final currentNode = queue.removeFirst();
-    result.add(currentNode.value);
 
-    for (final neighbor in currentNode.neighbors) {
-      if (!visited.contains(neighbor.value)) {
+    if (currentNode == destinationNode) {
+      // Reached the destination, reconstruct the path
+      return reconstructPath(previousNode, startNode, destinationNode);
+    }
+
+    for (final neighbor in graph[currentNode]!) {
+      if (!visited.contains(neighbor)) {
         queue.add(neighbor);
-        visited.add(neighbor.value);
+        visited.add(neighbor);
+        previousNode[neighbor] =
+            currentNode; // Store the previous node for path reconstruction
       }
     }
   }
 
-  return result;
+  // No path found
+  return [];
+}
+
+List<int> reconstructPath(
+    Map<int, int> previousNode, int startNode, int destinationNode) {
+  final path = <int>[];
+  var currentNode = destinationNode;
+  while (currentNode != startNode) {
+    path.add(currentNode);
+    currentNode = previousNode[currentNode]!;
+  }
+  path.add(startNode);
+  path.reversed;
+  return path;
 }
 
 void main() {
-  // Sample graph construction (modify as needed)
-  final node1 = Node(1, [
-    Node(2, []),
-    Node(3, [Node(4, [])])
-  ]);
-  final node2 = Node(2, [node1]);
-  final node3 = Node(3, [node1, Node(4, [])]);
-  final node4 = Node(4, [node3]);
+  // the graph
+  final Map<int, List<int>> graph = {
+    1: [2, 3],
+    2: [4, 5],
+    3: [4],
+    4: [],
+    5: [],
+  };
 
-  // Perform BFS traversal
-  final traversal = bfs(node2);
-  print(traversal.reversed); // Output: [1, 2, 3, 4] (order may vary)
+  final startNode = 2;
+  final destinationNode = 5;
+
+  final shortestPath = bfs(graph, startNode, destinationNode);
+
+  if (shortestPath.isEmpty) {
+    print('No path found from $startNode to $destinationNode');
+  } else {
+    print(
+        'Shortest path from $startNode to $destinationNode: ${shortestPath.reversed}');
+  }
 }
